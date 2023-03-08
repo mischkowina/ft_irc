7b71c13702750dbc44f8597c844f9349b1a5f6db
Saskia Mischnick <smischni@student.42wolfsburg.de>
2023-03-08 16:57:09 +0100

    Server backbone works :-)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -47,33 +47,11 @@ int main(int argc, char **argv)
 
 	std::cout << "Server running." << std::endl;
 
-	// char			buffer[4096];
-	// int				bytes_received = 0;
-	// int				bytes_sent = 0;
+	//map container to store all the client objects
 	ft::ClientMap	client_map;//typedef in irc.hpp
 	
 	while (true)
 	{
-		// loop to accept all new connections
-		// sockaddr_in	client_addr;//temporary variable / gets reused
-		// socklen_t	clientSize = sizeof(client_addr);//temporary variable / gets reused
-
-		// while (true)
-		// {
-		// 	int	clientSocket = accept(sockfd, (sockaddr *)&client_addr,	&clientSize);
-		// 	if (clientSocket == -1)
-		// 		break ;
-		// 	std::cout << "clientSock: " << clientSocket << std::endl;
-		// 	Client	newClient(clientSocket);
-		// 	newClient.setAddress(&client_addr);
-		// 	std::pair<ft::ClientMap::iterator, bool> insert_return = client_map.insert(make_pair(newClient.getAddress(), newClient));
-		// 	if (insert_return.second == false)
-		// 		std::cout << "Connection request failed: duplicate key." << std::endl;
-		// 	else
-		// 		std::cout << "Client succesfully connected from " << newClient.getAddress() << ". Total number of connected clients: " << client_map.size() << std::endl;
-		// }
-
-		
 		//create struct pollfd[] with the appropriate size (number of connected clients + 1 for the listening socket)
 		struct pollfd pollfds[client_map.size() + 1];
 
@@ -82,7 +60,7 @@ int main(int argc, char **argv)
 		pollfds[0].fd = sockfd;
 		pollfds[0].events = POLLIN;
 
-		//populate the rest of the struct pollfd[] with the client socket descriptors
+		//populate the rest of the struct pollfd[] with the client socket descriptors - doesn't happen until a connection is established
 		int i = 1;
 		for (ft::ClientMap::iterator it = client_map.begin(); it != client_map.end(); it++, i++)
 		{
@@ -100,94 +78,95 @@ int main(int argc, char **argv)
 		}
 		if (pollreturn == 0)
 			continue ;
-
-		int current_size = client_map.size() + 1;
-		for (int i = 0; i < current_size; i++)
+		
+		// check for error on listening socket
+		if (pollfds[0].revents & POLLERR || pollfds[0].revents & POLLHUP)
 		{
-			std::cout << "POLLFDS[" << i << "].revents: " << pollfds[i].revents << std::endl;
-			// if (pollfds[i].revents != POLLIN) //skip all pollfds without an event
-			// 	continue ;
-			// else if (pollfds[i].revents != POLLIN)//TBD if we want to protect against other events?
-			// {
-			// 	std::cerr << "ERROR: unexpected revent on fd " << pollfds[i].fd << std::endl;
-			// 	exit(1);
-			// }
-			if (pollfds[i].revents & POLLIN) //POLLIN event occured!
-			{
-				if (i == 0) //first fd, listening socket --> if this has an event, there is a new connection pending that has to be accpeted
-				{
-					sockaddr_in	client_addr;
-					socklen_t	clientSize = sizeof(client_addr);
-					int 		clientSocket;
+			std::cout << "Problem on listening socket?" << std::endl; //TBD if necessary
+		}
+		//check the listening socket for new pending connections
+		if (pollfds[0].revents & POLLIN)
+		{
+			sockaddr_in	client_addr;
+			socklen_t	clientSize = sizeof(client_addr);
+			int 		clientSocket;
 
-					//accept all new pending connections
-					while (true)
-					{
-						clientSocket = accept(sockfd, (sockaddr *)&client_addr,	&clientSize); 
-						if (clientSocket == -1)
-							break ; 
-						//if succesfull, create new client object for each connection 
-						Client	newClient(clientSocket);
-						//save address in object
-						newClient.setIP(&client_addr);
-						//insert new client into client map
-						std::pair<ft::ClientMap::iterator, bool> insert_return = client_map.insert(make_pair(newClient.getKey(), newClient));
-						//if there is already a client with the same ip adress + socket, insertion fails
-						if (insert_return.second == false)
-						{
-							std::cout << "Connection request failed: duplicate key." << std::endl;
-							close(newClient.getSocket());
-						}
-						else
-							std::cout << "Client succesfully connected from " << newClient.getIP() << " on socket " << newClient.getSocket() << ". Total number of connected clients: " << client_map.size() << std::endl;
-					}
+			//accept all new pending connections
+			while (true)
+			{
+				clientSocket = accept(sockfd, (sockaddr *)&client_addr,	&clientSize); 
+				if (clientSocket == -1)
+					break ; 
+				//if succesfull, create new client object for the connection 
+				Client	newClient(clientSocket);
+				//save address in object
+				newClient.setIP(&client_addr);
+				//insert new client into client map
+				std::pair<ft::ClientMap::iterator, bool> insert_return = client_map.insert(make_pair(newClient.getKey(), newClient));
+				//if there is already a client with the same ip adress + socket, insertion fails
+				if (insert_return.second == false)
+				{
+					std::cout << "Connection request failed: duplicate key." << std::endl;
+					close(newClient.getSocket());
+				}
+				else
+					std::cout << "Client succesfully connected from " << newClient.getIP() << " on socket " << newClient.getSocket() << ". Total number of connected clients: " << client_map.size() << std::endl;
+			}
+		}
+
+		//check all other sockets (of the clients) for events
+		i = 1;
+		for (ft::ClientMap::iterator it = client_map.begin(); it != client_map.end(); it++, i++)
+		{
+			//skip all sockets without an event
+			if (pollfds[i].revents == 0)
+				continue ;
+			//check socket for error events, if yes close the connection and delete the client from the map
+			if (pollfds[i].revents & POLLERR || pollfds[i].revents & POLLHUP)
+			{
+				std::cout << "Connection with " << it->second.getIP() << " on socket " << it->second.getSocket() << " lost." << std::endl;
+				close(pollfds[i].fd);
+				client_map.erase(it);
+				continue;
+			}
+			//check for POLLIN events
+			if (pollfds[i].revents & POLLIN)
+			{
+				char	buffer[1024]; //TBD: max size of messages
+				
+				//receive message from socket
+				int recv_return = recv(pollfds[i].fd, buffer, sizeof(buffer), 0);
+				if (recv_return < 0)
+				{
+					std::cerr << "ERROR on recv" << std::endl;
+					exit(1);
+				}
+				//if recv returns 0, the connection has been closed/lost on the client side -> close connection and delete client
+				else if (recv_return == 0)
+				{
+					std::cout << "Connection with " << it->second.getIP() << " on socket " << it->second.getSocket() << " lost." << std::endl;
+					close(pollfds[i].fd);
+					client_map.erase(it);
+					continue; 
 				}
 				else
 				{
-					while (true)
+					//Display received message in terminal
+					std::cout << "Message received: " << buffer << std::endl;
+					//echo that message back to the client who send it
+					int send_return = send(pollfds[i].fd, buffer, recv_return, 0);
+					if (send_return < 0)
 					{
-						char	buffer[80]; //TBD: max size of messages
-						int recv_return = recv(pollfds[i].fd, buffer, sizeof(buffer), 0);//TBD if we need any flag for recv?
-						if (recv_return < 0)//TBD ob exit oder lieber nur break ;
-							break;
-						else if (recv_return == 0)//connection has been closed - OPEN: close the fd, remove the client object
-							break;
-						else
-							std::cout << "Message received: " << buffer << std::endl; 
+						std::cerr << "ERROR on send" << std::endl;
+						std::cout << "Closing connection with " << it->second.getIP() << " on socket " << it->second.getSocket() << " ." << std::endl;
+						close(pollfds[i].fd);
+						client_map.erase(it);
 					}
 				}
 			}
 		}
-
-		
-		// //clear the buffer and set the clientSocket to non-blocking
-		// bzero(buffer, sizeof(buffer));
-		// int n = fcntl(clientSocket, F_SETFL, O_NONBLOCK);
-		// if (n == -1) {
-		// 	std::cerr << "ERROR on fcntl" << std::endl;
-		// 	exit(1);
-		// }
-
-		// // receive data from a client
-		// bytes_received = recv(sockfd, buffer, sizeof(buffer), 0);
-		// if (bytes_received == -1) {
-		// 	std::cerr << "ERROR reading from socket" << std::endl;
-		// 	close(sockfd);
-		// 	exit(1);
-		// }
-
-		// // forward data to other clients
-		// bytes_sent = send(clientSocket, buffer, sizeof(buffer), 0);
-		// if (bytes_sent == -1)
-		// 	exit(1);
-
-		// getsockname(clientSocket, (struct sockaddr *)&client_addr, &clientSize);
-		// std::cout << "Here is the message: " << buffer << " :from client " 
-		// 		<< inet_ntoa(client_addr.sin_addr) << std::endl;
-
 	}
-
-	// close(clientSocket); //moved to Client destructor
+	//TO-DO: close all client sockets
 	close(sockfd);
 
 	return 0;
